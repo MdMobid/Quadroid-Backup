2153d0aedc5067232bace1dff373835fce0b0e68
NatZerg <130333798+NatZerg@users.noreply.github.com>
2023-04-10 12:07:12 +0530

    voice module ka code chipkana hai isko

--- send.py
+++ send.py
@@ -1,115 +1,29 @@
-import openai
-import speech_recognition as sr
-import pyttsx3
-import time
-from playsound import playsound
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+pe=15
+rec=14
+GPIO.setup(pe,GPIO.OUT)
+GPIO.setup(rec,GPIO.OUT)
+GPIO.output(pe,0)
+GPIO.output(rec,0)
+def aufnahme():
+ sleep(3)
+ print("Aufnahme startet")
+ GPIO.output(pe, 1)
+ sleep(10)
+ GPIO.output(pe, 0)
+ sleep(5)
+ print("Aufnahme beendet")
+def abspielen():
+ print("Wiedergabe startet")
+ GPIO.output(rec, 1)
+ sleep(1)
+ GPIO.output(rec, 0)
+ sleep(10)
+ print("Wiedergabe beendet")
 
- 
-x=0 #a fleg to check if control is out of assestent
-ll=[] #
-go=False # to check if weak up call is done
-c1=False #to allow first question after weak up call
-# Initialize OpenAI API
-openai.api_key = "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"
-# Initialize the text to speech engine 
-engine=pyttsx3.init()
-
-
-
-mss=[{'role':'system','content':'you are a smart ai assistant and your name is Dodo ,and you are created by KV Number 1 Saltlake Curious Squad'}]
-
-def generate_response(prompt):
-    
-    mss.append(
-        {"role": "user", "content": prompt},
-    )
-    chat = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=mss
-    )
-    reply = chat.choices[0].message.content
-    mss.append({"role": "assistant", "content": reply})
-    
-
-    return reply
-def speak_text(text):
-    engine.say(text)
-    engine.runAndWait()
-
-def main():
-    global go
-    global c1
-    global x
-    while True:
-        tt=time.time()
-        if go ==False:
-         if x ==0:
-             print()
-             playsound("sound effect\\mixkit-retro-game-notification-212.wav")
-         with sr.Microphone() as source:
-            recognizer = sr.Recognizer()
-            audio = recognizer.listen(source)
-            x=1
-        try:
-            if go == False:
-             
-                 
-             transcription = recognizer.recognize_google(audio)
-        
-             if "hey" in transcription.lower() :  
-                playsound("sound effect\\the-notification-email-143029.mp3")
-                go=True
-                c1=True
-                tt1=time.time() 
-                #time.sleep(2)
-                
-            while go: 
-                playsound("sound effect\\interface-124464.mp3") 
-                print("Say Something...")
-                speak_text("Say Something")
-                c2=int(time.time()-tt1)
-                
-                if c2>20:
-                    go=False
-                    x=0  
-                filename = "input.wav"
-                   
-                with sr.Microphone() as source:
-                 recognizer = sr.Recognizer()
-                 source.pause_threshold = 20
-                 audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
-                 with open(filename, "wb") as f:
-                     f.write(audio.get_wav_data())
-                audio_file= open("input.wav", "rb")
-                transcript = openai.Audio.translate("whisper-1", audio_file)
-                text=transcript["text"]
-                cc=text.split()
-                print("tttex",tt-time.time())
-
-                if text == "Thank you." :
-                    print('ttt')
-                    continue
-
-                if text:
-                    print("Got it..")
-                    playsound("sound effect\\mixkit-retro-game-notification-212.wav")
-                    tt1=time.time()
-                    c1=False
-                    print(f"You said: {text}")
-                    tt=time.time()
-
-                    # generate the response
-                    response = generate_response(text)
-                    print(f"Assistant: {response}")
-                    print(tt-time.time())
-
-
-                    # read response using GPT-3
-                    speak_text(response)
-                    print("Listening...")
-
-        except Exception as e:
-                speak_text("Didn't Got That, Please Try Again")
-                print("An error occurred: {}".format(e))
-
-if __name__=="__main__":
-    main()
+while True:
+ aufnahme()
+ abspielen()
